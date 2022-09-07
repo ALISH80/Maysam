b5c204500546daaf60c66102a9032df8baf45dd9
atoofihub <atoofi_alireza@yahoo.com>
2022-09-08 01:10:04 +0430

    edit ID and change int with classes.please dont override on it

--- Js/javascript.js
+++ Js/javascript.js
@@ -2,14 +2,16 @@ const open = document.querySelector("#hamburger");
 open.addEventListener("click", openLeftBar);
 function openLeftBar() {
   const Left = document.querySelector(".Left");
-  Left.id = "open";
+  Left.classList.remove("Close");
+  Left.classList.add("open");
 }
 
 const close = document.querySelector("#close");
 close.addEventListener("click", closeLeftBar);
 function closeLeftBar() {
   const Left = document.querySelector(".Left");
-  Left.id = "Close";
+  Left.classList.remove("open");
+  Left.classList.add("Close");
 }
 
 
@@ -17,12 +19,14 @@ const opennav = document.querySelector("#right-hamburger");
 opennav.addEventListener("click", openNavBar);
 function openNavBar() {
   const Topnav = document.querySelector(".Heading");
-  Topnav.id = "openNav";
+  Topnav.classList.remove("CloseNav");
+  Topnav.classList.add("openNav");
 }
 
 const closenav = document.querySelector("#closeNav");
 closenav.addEventListener("click", closeNavBar);
 function closeNavBar() {
   const Topnav = document.querySelector(".Heading");
-  Topnav.id = "CloseNav";
+  Topnav.classList.remove("openNav");
+  Topnav.classList.add("CloseNav");
 }
